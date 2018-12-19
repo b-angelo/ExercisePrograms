0dca7167d196ba7251f0f5a7f18d5392a5f8b6e5
Ben Angelo <ben.t.angelo@gmail.com>
2018-12-19 00:22:38 -0600

    commit

--- ExerciseProgram.Api/Services/SubscriberProfileService.cs
+++ ExerciseProgram.Api/Services/SubscriberProfileService.cs
@@ -107,7 +107,7 @@ namespace ExerciseProgram.Api.Services
                 WorkoutCompletedYear = workoutsCompletedYear,
                 CurrentWorkoutExerciseCount = excerciseCount
             };
-
+            
             return profile;
 
         }
